3d964bed5026c188880f12f0a036d1e8a019ed44
Conner <chuckieminster@aol.com>
2022-12-15 13:04:18 +0000

    Added vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,15 @@
 import '../styles/scss/main.scss';
 import '@fortawesome/fontawesome-svg-core/styles.css'
 
+import { Analytics } from '@vercel/analytics/react';
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  )
 }
 
 export default MyApp
